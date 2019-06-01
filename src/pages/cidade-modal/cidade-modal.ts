0988cb7e44470a51e5e0c9e46c3f642d666ce9c7
Eduardo Simão <edu-simao@outlook.com>
2019-06-01 11:33:04 -0300

    botões de vacinas no modal de cidade

--- src/pages/cidade-modal/cidade-modal.ts
+++ src/pages/cidade-modal/cidade-modal.ts
@@ -18,6 +18,12 @@ export class CidadeModalPage {
   nome_cidade;
   uf_cidade;
 
+  cardDengue: Boolean;
+  cardLep: Boolean;
+  hiddentxt: Boolean;
+  propBtn1: string;
+  propBtn2: string;
+
   constructor(public navCtrl: NavController, 
     public navParams: NavParams,
     public viewCtrl : ViewController) {
@@ -27,6 +33,12 @@ export class CidadeModalPage {
         this.nome_cidade = this.navParams.data.nome_cidade;
         this.uf_cidade = this.navParams.data.uf_cidade;
       }
+
+      this.cardDengue = true;
+      this.cardLep = true;
+      this.hiddentxt = false;
+      this.propBtn1 = "colunas2";
+      this.propBtn2 = "colunas2";
   }
 
   ionViewDidLoad() {
@@ -36,5 +48,21 @@ export class CidadeModalPage {
   public closeModal(){
     this.viewCtrl.dismiss();
   }
+  public showDengue(){
+    this.propBtn1 = "colunas2";
+    this.propBtn2 = "colunas1";
+    this.hiddentxt = true;
+    this.cardLep = true;
+    this.cardDengue = false;
+  } 
+
+  public showLep(){
+    this.propBtn1 = "colunas1";
+    this.propBtn2 = "colunas2";
+    this.hiddentxt = true;
+    this.cardLep = false;
+    this.cardDengue = true;
+} 
+
 
 }
